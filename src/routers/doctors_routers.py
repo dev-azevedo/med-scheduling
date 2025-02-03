44ba062e3598d9fe098068bea65b5eefde95ff18
Jhonatan Azevedo <dev.azevedo@outlook.com>
2025-02-03 00:30:03 -0300

    ADD: summary in routes

--- src/routers/doctors_routers.py
+++ src/routers/doctors_routers.py
@@ -10,35 +10,35 @@ router = APIRouter(prefix='/doctors', tags=["Doctors"])
 def get_doctor_service(db: Session = Depends(database.get_db)):
     return service.DoctorService(db=db)
 
-@router.post("/", response_model=schemas.Doctor)
+@router.post("/", response_model=schemas.Doctor, summary="Create a new doctor")
 def post(doctor: schemas.DoctorCreate, service: service.DoctorService = Depends(get_doctor_service)):
     try:
         return service.create(doctor=doctor)
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
     
-@router.get("/", response_model=list[schemas.Doctor])
+@router.get("/", response_model=list[schemas.Doctor], summary="Get all doctors")
 def get(skip: int = 1, limit: int = 10, service: service.DoctorService = Depends(get_doctor_service)):
     try:
         return service.get(skip=skip, limit=limit)
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
-@router.get("/{doctor_id}", response_model=schemas.Doctor)
+@router.get("/{doctor_id}", response_model=schemas.Doctor, summary="Get a doctor by id")
 def get_id(doctor_id: int, service: service.DoctorService = Depends(get_doctor_service)):
     try:
         return service.get_id(doctor_id=doctor_id)
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
-@router.put("/{doctor_id}", response_model=schemas.Doctor)
+@router.put("/{doctor_id}", response_model=schemas.Doctor, summary="Update a doctor by id")
 def put(doctor_id: int, doctor: schemas.DoctorUpdate, service: service.DoctorService = Depends(get_doctor_service)):
     try:
         return service.update(id=doctor_id, doctor=doctor)
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
-@router.delete("/{doctor_id}", response_model=schemas.Doctor)
+@router.delete("/{doctor_id}", response_model=schemas.Doctor, summary="Delete a doctor by id")
 def delete(doctor_id: int, service: service.DoctorService = Depends(get_doctor_service)):
     try:
         return service.delete(doctor_id=doctor_id)
--- src/routers/medical_specialties_routers.py
+++ src/routers/medical_specialties_routers.py
@@ -10,35 +10,35 @@ router = APIRouter(prefix='/medical-specialties', tags=["Medical Specialties"])
 def get_medical_specialty_service(db: Session = Depends(database.get_db)):
     return service.MedicalSpecialtyService(db=db)
 
-@router.post("/", response_model=schemas.MedicalSpecialties)
+@router.post("/", response_model=schemas.MedicalSpecialties, summary="Create a new medical specialty")
 def post(medical_specialty: schemas.MedicalSpecialtiesCreate, service: service.MedicalSpecialtyService = Depends(get_medical_specialty_service)):
     try:
         return service.create(medical_specialty=medical_specialty)
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
     
-@router.get("/", response_model=List[schemas.MedicalSpecialties])
+@router.get("/", response_model=List[schemas.MedicalSpecialties], summary="Get all medical specialties")
 def get(skip: int = 1, limit: int = 10, service: service.MedicalSpecialtyService = Depends(get_medical_specialty_service)):
     try:
         return service.get(skip=skip, limit=limit)
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
-@router.get("/{medical_specialty_id}", response_model=schemas.MedicalSpecialties)
+@router.get("/{medical_specialty_id}", response_model=schemas.MedicalSpecialties, summary="Get a medical specialty by id")
 def get_id(medical_specialty_id: int, service: service.MedicalSpecialtyService = Depends(get_medical_specialty_service)):
     try:
         return service.get_id(medical_specialty_id=medical_specialty_id)
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
-@router.put("/{medical_specialty_id}", response_model=schemas.MedicalSpecialties)
+@router.put("/{medical_specialty_id}", response_model=schemas.MedicalSpecialties, summary="Update a medical specialty by id")
 def put(medical_specialty_id: int, medical_specialty: schemas.MedicalSpecialtiesUpdate, service: service.MedicalSpecialtyService = Depends(get_medical_specialty_service)):
     try:
         return service.update(id=medical_specialty_id, medical_specialty=medical_specialty)
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
-@router.delete("/{medical_specialty_id}", response_model=schemas.MedicalSpecialties)
+@router.delete("/{medical_specialty_id}", response_model=schemas.MedicalSpecialties, summary="Delete a medical specialty by id")
 def delete(medical_specialty_id: int, service: service.MedicalSpecialtyService = Depends(get_medical_specialty_service)):
     try:
         return service.delete(medical_specialty_id=medical_specialty_id)
